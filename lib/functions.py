077908874fde5b3595f4ab12a7048af1a9f7f78d
Rotichgideon <gideonrotich2001@gmail.com>
2024-06-01 10:55:17 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    print (result)
+    return result
+    print(result)
 
 def halve(number):
-    pass
+    result ="null" if not isinstance(number, (int, float)) else number /2
+    print (result)
+    return result
+
+
+    greet_programmer()
+    greet("Naureen")
+    greet_with_default()
+    greet_with_default("Naureen")
+    add(3,2)
+    halve("j")
